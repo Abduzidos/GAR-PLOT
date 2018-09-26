944ac0a547aac40ad18363186acf59ef419116f0
Caio Cordeiro <ccmr@cesar.school>
2018-09-26 13:41:34 -0300

    Update criar.js

--- criar.js
+++ criar.js
@@ -1,16 +1,15 @@
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
-var moment = require('moment');
-
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
+var moment = require("moment");
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar'];
-const TOKEN_PATH = 'token.json';
+const SCOPES = ["https://www.googleapis.com/auth/calendar"];
+const TOKEN_PATH = "token.json";
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
+fs.readFile("credentials.json", (err, content) => {
+  if (err) return console.log("Error loading client secret file:", err);
   // Authorize a client with credentials, then call the Google Calendar API.
   authorize(JSON.parse(content), listEvents);
 });
@@ -22,9 +21,12 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
@@ -42,23 +44,23 @@ function authorize(credentials, callback) {
  */
 function getAccessToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
+    access_type: "offline",
+    scope: SCOPES
   });
-  console.log('Authorize this app by visiting this url:', authUrl);
+  console.log("Authorize this app by visiting this url:", authUrl);
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
+    output: process.stdout
   });
-  rl.question('Enter the code from that page here: ', (code) => {
+  rl.question("Enter the code from that page here: ", code => {
     rl.close();
     oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
+      if (err) return console.error("Error retrieving access token", err);
       oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
         if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
+        console.log("Token stored to", TOKEN_PATH);
       });
       callback(oAuth2Client);
     });
@@ -70,71 +72,73 @@ function getAccessToken(oAuth2Client, callback) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  var starttime= moment();
-  var endtime= starttime.clone().add(1,'hour')
-  starttime.format()
-  endtime.format()
-  var email = 'ccmr@cesar.school'
-  const timeZone = 'America/Bahia'
+  const calendar = google.calendar({ version: "v3", auth });
+  var starttime = moment();
+  var endtime = starttime.clone().add(1, "hour");
+  starttime.format();
+  endtime.format();
+  var email = "ccmr@cesar.school";
+  const timeZone = "America/Bahia";
 
   var event = {
-    'summary': 'OI THIAGO',
-    'location': '77 Cais do Apolo, Recife, PE',
-    'description': 'TESTE',
-    'start': {
-      'dateTime': starttime,
-      'timeZone': timeZone, 
+    summary: "OI THIAGO",
+    location: "77 Cais do Apolo, Recife, PE",
+    description: "TESTE",
+    start: {
+      dateTime: starttime,
+      timeZone: timeZone
     },
-    'end': {
-      'dateTime': endtime,
-      'timeZone': timeZone,
+    end: {
+      dateTime: endtime,
+      timeZone: timeZone
     },
-    'recurrence': [
-      'RRULE:FREQ=DAILY;COUNT=1'
-    ],
-    'attendees': [
-      {'email': email},
-    ],
-    'reminders': {
-      'useDefault': false,
-      'overrides': [
-        {'method': 'email', 'minutes': 24 * 60},
-        {'method': 'popup', 'minutes': 10},
-      ],
-    },
+    recurrence: ["RRULE:FREQ=DAILY;COUNT=1"],
+    attendees: [{ email: email }],
+    reminders: {
+      useDefault: false,
+      overrides: [
+        { method: "email", minutes: 24 * 60 },
+        { method: "popup", minutes: 10 }
+      ]
+    }
   };
-  
-  calendar.events.insert({
-    auth: auth,
-    calendarId: 'primary',
-    resource: event,
-  }, function(err, event) {
-    if (err) {
-      console.log('There was an error contacting the Calendar service: ' + err);
-      return;
+
+  calendar.events.insert(
+    {
+      auth: auth,
+      calendarId: "primary",
+      resource: event
+    },
+    function(err, event) {
+      if (err) {
+        console.log(
+          "There was an error contacting the Calendar service: " + err
+        );
+        return;
+      }
+      console.log("Event created: %s", event.htmlLink);
     }
-    console.log('Event created: %s', event.htmlLink);
-  });
-  console.log(calendar.events)
-  
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
+  );
+  calendar.events.list(
+    {
+      calendarId: "primary",
+      timeMin: new Date().toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: "startTime"
+    },
+    (err, res) => {
+      if (err) return console.log("The API returned an error: " + err);
+      const events = res.data.items;
+      if (events.length) {
+        console.log("Upcoming 10 events:");
+        events.map((event, i) => {
+          const start = event.start.dateTime || event.start.date;
+          console.log(`${start} - ${event.summary}`);
+        });
+      } else {
+        console.log("No upcoming events found.");
+      }
     }
-  });
+  );
 }
